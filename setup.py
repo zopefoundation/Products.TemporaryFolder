b325b278e597af559bbb73b22f3bc1be2f09f445
Hanno Schlichting <hanno@hannosch.eu>
2016-08-02 11:18:33 +0200

    vb [ci skip]

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Products.TemporaryFolder',
-    version='3.0',
+    version='3.1.dev0',
     url='https://pypi.python.org/pypi/Products.TemporaryFolder',
     license='ZPL 2.1',
     description="Zope 2 temporary folder support.",
